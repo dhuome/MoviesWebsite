0665d05768a1c52c8a9052d1358f2f913fc37655
Abdulrahman Bin Rushud <91965409+dhuome@users.noreply.github.com>
2022-02-07 20:02:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,493 @@
+const open_wandavision = document.getElementById("open-wandavision");
+const wandavision = document.getElementById("wandavision");
+const close_wandavision = document.getElementById("close_wandavision");
+open_wandavision.addEventListener("click", () => {
+  wandavision.classList.add("show");
+});
+close_wandavision.addEventListener("click", () => {
+  wandavision.classList.remove("show");
+});
+
+const open_thefalcon = document.getElementById("open-thefalcon");
+const thefalcon = document.getElementById("thefalcon");
+const close_thefalcon = document.getElementById("close_thefalcon");
+open_thefalcon.addEventListener("click", () => {
+  thefalcon.classList.add("show");
+});
+close_thefalcon.addEventListener("click", () => {
+  thefalcon.classList.remove("show");
+});
+
+const open_loki = document.getElementById("open-loki");
+const loki = document.getElementById("loki");
+const close_loki = document.getElementById("close_loki");
+open_loki.addEventListener("click", () => {
+  loki.classList.add("show");
+});
+close_loki.addEventListener("click", () => {
+  loki.classList.remove("show");
+});
+
+const open_hawkeye = document.getElementById("open-hawkeye");
+const hawkeye = document.getElementById("hawkeye");
+const close_hawkeye = document.getElementById("close_hawkeye");
+open_hawkeye.addEventListener("click", () => {
+  hawkeye.classList.add("show");
+});
+close_hawkeye.addEventListener("click", () => {
+  hawkeye.classList.remove("show");
+});
+
+const open_spider3 = document.getElementById("open-spider3");
+const spider3 = document.getElementById("spider3");
+const close_spider3 = document.getElementById("close_spider3");
+open_spider3.addEventListener("click", () => {
+  spider3.classList.add("show");
+});
+close_spider3.addEventListener("click", () => {
+  spider3.classList.remove("show");
+});
+
+const open_eternals = document.getElementById("open-eternals");
+const eternals = document.getElementById("eternals");
+const close_eternals = document.getElementById("close_eternals");
+open_eternals.addEventListener("click", () => {
+  eternals.classList.add("show");
+});
+close_eternals.addEventListener("click", () => {
+  eternals.classList.remove("show");
+});
+
+const open_spider2 = document.getElementById("open-spider2");
+const spider2 = document.getElementById("spider2");
+const close_spider2 = document.getElementById("close_spider2");
+open_spider2.addEventListener("click", () => {
+  spider2.classList.add("show");
+});
+close_spider2.addEventListener("click", () => {
+  spider2.classList.remove("show");
+});
+
+const open_shangchi = document.getElementById("open-shangchi");
+const shangchi = document.getElementById("shangchi");
+const close_shangchi = document.getElementById("close_shangchi");
+open_shangchi.addEventListener("click", () => {
+  shangchi.classList.add("show");
+});
+close_shangchi.addEventListener("click", () => {
+  shangchi.classList.remove("show");
+});
+
+const open_endgame = document.getElementById("open-endgame");
+const endgame = document.getElementById("endgame");
+const close_endgame = document.getElementById("close_endgame");
+open_endgame.addEventListener("click", () => {
+  endgame.classList.add("show");
+});
+close_endgame.addEventListener("click", () => {
+  endgame.classList.remove("show");
+});
+
+const open_antman2 = document.getElementById("open-antman2");
+const antman2 = document.getElementById("antman2");
+const close_antman2 = document.getElementById("close_antman2");
+open_antman2.addEventListener("click", () => {
+  antman2.classList.add("show");
+});
+close_antman2.addEventListener("click", () => {
+  antman2.classList.remove("show");
+});
+
+const open_infinitywar = document.getElementById("open-infinitywar");
+const infinitywar = document.getElementById("infinitywar");
+const close_infinitywar = document.getElementById("close_infinitywar");
+open_infinitywar.addEventListener("click", () => {
+  infinitywar.classList.add("show");
+});
+close_infinitywar.addEventListener("click", () => {
+  infinitywar.classList.remove("show");
+});
+
+const open_thor3 = document.getElementById("open-thor3");
+const thor3 = document.getElementById("thor3");
+const close_thor3 = document.getElementById("close_thor3");
+open_thor3.addEventListener("click", () => {
+  thor3.classList.add("show");
+});
+close_thor3.addEventListener("click", () => {
+  thor3.classList.remove("show");
+});
+
+const open_blackpanther = document.getElementById("open-blackpanther");
+const blackpanther = document.getElementById("blackpanther");
+const close_blackpanther = document.getElementById("close_blackpanther");
+open_blackpanther.addEventListener("click", () => {
+  blackpanther.classList.add("show");
+});
+close_blackpanther.addEventListener("click", () => {
+  blackpanther.classList.remove("show");
+});
+
+const open_doctorstrange = document.getElementById("open-doctorstrange");
+const doctorstrange = document.getElementById("doctorstrange");
+const close_doctorstrange = document.getElementById("close_doctorstrange");
+open_doctorstrange.addEventListener("click", () => {
+  doctorstrange.classList.add("show");
+});
+close_doctorstrange.addEventListener("click", () => {
+  doctorstrange.classList.remove("show");
+});
+
+const open_spider1 = document.getElementById("open-spider1");
+const spider1 = document.getElementById("spider1");
+const close_spider1 = document.getElementById("close_spider1");
+open_spider1.addEventListener("click", () => {
+  spider1.classList.add("show");
+});
+close_spider1.addEventListener("click", () => {
+  spider1.classList.remove("show");
+});
+
+const open_blackwidow = document.getElementById("open-blackwidow");
+const blackwidow = document.getElementById("blackwidow");
+const close_blackwidow = document.getElementById("close_blackwidow");
+open_blackwidow.addEventListener("click", () => {
+  blackwidow.classList.add("show");
+});
+close_blackwidow.addEventListener("click", () => {
+  blackwidow.classList.remove("show");
+});
+
+const open_captain3 = document.getElementById("open-captain3");
+const captain3 = document.getElementById("captain3");
+const close_captain3 = document.getElementById("close_captain3");
+open_captain3.addEventListener("click", () => {
+  captain3.classList.add("show");
+});
+close_captain3.addEventListener("click", () => {
+  captain3.classList.remove("show");
+});
+
+const open_antman1 = document.getElementById("open-antman1");
+const antman1 = document.getElementById("antman1");
+const close_antman1 = document.getElementById("close_antman1");
+open_antman1.addEventListener("click", () => {
+  antman1.classList.add("show");
+});
+close_antman1.addEventListener("click", () => {
+  antman1.classList.remove("show");
+});
+
+const open_ageofultron = document.getElementById("open-ageofultron");
+const ageofultron = document.getElementById("ageofultron");
+const close_ageofultron = document.getElementById("close_ageofultron");
+open_ageofultron.addEventListener("click", () => {
+  ageofultron.classList.add("show");
+});
+close_ageofultron.addEventListener("click", () => {
+  ageofultron.classList.remove("show");
+});
+
+const open_guardians2 = document.getElementById("open-guardians2");
+const guardians2 = document.getElementById("guardians2");
+const close_guardians2 = document.getElementById("close_guardians2");
+open_guardians2.addEventListener("click", () => {
+  guardians2.classList.add("show");
+});
+close_guardians2.addEventListener("click", () => {
+  guardians2.classList.remove("show");
+});
+
+const open_guardians1 = document.getElementById("open-guardians1");
+const guardians1 = document.getElementById("guardians1");
+const close_guardians1 = document.getElementById("close_guardians1");
+open_guardians1.addEventListener("click", () => {
+  guardians1.classList.add("show");
+});
+close_guardians1.addEventListener("click", () => {
+  guardians1.classList.remove("show");
+});
+
+const open_captain2 = document.getElementById("open-captain2");
+const captain2 = document.getElementById("captain2");
+const close_captain2 = document.getElementById("close_captain2");
+open_captain2.addEventListener("click", () => {
+  captain2.classList.add("show");
+});
+close_captain2.addEventListener("click", () => {
+  captain2.classList.remove("show");
+});
+
+const open_thor2 = document.getElementById("open-thor2");
+const thor2 = document.getElementById("thor2");
+const close_thor2 = document.getElementById("close_thor2");
+open_thor2.addEventListener("click", () => {
+  thor2.classList.add("show");
+});
+close_thor2.addEventListener("click", () => {
+  thor2.classList.remove("show");
+});
+
+const open_ironman3 = document.getElementById("open-ironman3");
+const ironman3 = document.getElementById("ironman3");
+const close_ironman3 = document.getElementById("close_ironman3");
+open_ironman3.addEventListener("click", () => {
+  ironman3.classList.add("show");
+});
+close_ironman3.addEventListener("click", () => {
+  ironman3.classList.remove("show");
+});
+
+const open_avengers = document.getElementById("open-avengers");
+const avengers = document.getElementById("avengers");
+const close_avengers = document.getElementById("close_avengers");
+open_avengers.addEventListener("click", () => {
+  avengers.classList.add("show");
+});
+close_avengers.addEventListener("click", () => {
+  avengers.classList.remove("show");
+});
+
+const open_thor1 = document.getElementById("open-thor1");
+const thor1 = document.getElementById("thor1");
+const close_thor1 = document.getElementById("close_thor1");
+open_thor1.addEventListener("click", () => {
+  thor1.classList.add("show");
+});
+close_thor1.addEventListener("click", () => {
+  thor1.classList.remove("show");
+});
+
+const open_hulk = document.getElementById("open-hulk");
+const hulk = document.getElementById("hulk");
+const close_hulk = document.getElementById("close_hulk");
+open_hulk.addEventListener("click", () => {
+  hulk.classList.add("show");
+});
+close_hulk.addEventListener("click", () => {
+  hulk.classList.remove("show");
+});
+
+const open_ironman2 = document.getElementById("open-ironman2");
+const ironman2 = document.getElementById("ironman2");
+const close_ironman2 = document.getElementById("close_ironman2");
+open_ironman2.addEventListener("click", () => {
+  ironman2.classList.add("show");
+});
+close_ironman2.addEventListener("click", () => {
+  ironman2.classList.remove("show");
+});
+
+const open_ironman1 = document.getElementById("open-ironman1");
+const ironman1 = document.getElementById("ironman1");
+const close_ironman1 = document.getElementById("close_ironman1");
+open_ironman1.addEventListener("click", () => {
+  ironman1.classList.add("show");
+});
+close_ironman1.addEventListener("click", () => {
+  ironman1.classList.remove("show");
+});
+
+const open_marvel = document.getElementById("open-marvel");
+const marvel = document.getElementById("marvel");
+const close_marvel = document.getElementById("close_marvel");
+open_marvel.addEventListener("click", () => {
+  marvel.classList.add("show");
+});
+close_marvel.addEventListener("click", () => {
+  marvel.classList.remove("show");
+});
+
+const open_captain1 = document.getElementById("open-captain1");
+const captain1 = document.getElementById("captain1");
+const close_captain1 = document.getElementById("close_captain1");
+open_captain1.addEventListener("click", () => {
+  captain1.classList.add("show");
+});
+close_captain1.addEventListener("click", () => {
+  captain1.classList.remove("show");
+});
+
+// Set the date we're counting down to
+var countDown1 = new Date("Mar 31, 2022").getTime();
+var countDown2 = new Date("Feb 17, 2023").getTime();
+var countDown3 = new Date("Jul 8, 2022").getTime();
+var countDown4 = new Date("Mar 30, 2022").getTime();
+var countDown5 = new Date("Jan 1, 2023").getTime();
+var countDown6 = new Date("Jul 28, 2023").getTime();
+var countDown7 = new Date("May 5, 2023").getTime();
+var countDown8 = new Date("Jan 1, 2023").getTime();
+var countDown9 = new Date("Nov 9, 2022").getTime();
+var countDown10 = new Date("May 5, 2022").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance1 = countDown1 - now;
+  var distance2 = countDown2 - now;
+  var distance3 = countDown3 - now;
+  var distance4 = countDown4 - now;
+  var distance5 = countDown5 - now;
+  var distance6 = countDown6 - now;
+  var distance7 = countDown7 - now;
+  var distance8 = countDown8 - now;
+  var distance9 = countDown9 - now;
+  var distance10 = countDown10 - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days1 = Math.floor(distance1 / (1000 * 60 * 60 * 24));
+  var hours1 = Math.floor(
+    (distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes1 = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds1 = Math.floor((distance1 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+  var hours2 = Math.floor(
+    (distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days3 = Math.floor(distance3 / (1000 * 60 * 60 * 24));
+  var hours3 = Math.floor(
+    (distance3 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes3 = Math.floor((distance3 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds3 = Math.floor((distance3 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days4 = Math.floor(distance4 / (1000 * 60 * 60 * 24));
+  var hours4 = Math.floor(
+    (distance4 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes4 = Math.floor((distance4 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds4 = Math.floor((distance4 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days5 = Math.floor(distance5 / (1000 * 60 * 60 * 24));
+  var hours5 = Math.floor(
+    (distance5 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes5 = Math.floor((distance5 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds5 = Math.floor((distance5 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days6 = Math.floor(distance6 / (1000 * 60 * 60 * 24));
+  var hours6 = Math.floor(
+    (distance6 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes6 = Math.floor((distance6 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds6 = Math.floor((distance6 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days7 = Math.floor(distance7 / (1000 * 60 * 60 * 24));
+  var hours7 = Math.floor(
+    (distance7 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes7 = Math.floor((distance7 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds7 = Math.floor((distance7 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days8 = Math.floor(distance8 / (1000 * 60 * 60 * 24));
+  var hours8 = Math.floor(
+    (distance8 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes8 = Math.floor((distance8 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds8 = Math.floor((distance8 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days9 = Math.floor(distance9 / (1000 * 60 * 60 * 24));
+  var hours9 = Math.floor(
+    (distance9 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes9 = Math.floor((distance9 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds9 = Math.floor((distance9 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  var days10 = Math.floor(distance10 / (1000 * 60 * 60 * 24));
+  var hours10 = Math.floor(
+    (distance10 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes10 = Math.floor((distance10 % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds10 = Math.floor((distance10 % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+
+  // Output the result in an element with id="demo"
+  document.getElementById("time1").innerHTML =
+    days1 + "d " + hours1 + "h " + minutes1 + "m " + seconds1 + "s ";
+
+  document.getElementById("time2").innerHTML =
+    days2 + "d " + hours2 + "h " + minutes2 + "m " + seconds2 + "s ";
+
+  document.getElementById("time3").innerHTML =
+    days3 + "d " + hours3 + "h " + minutes3 + "m " + seconds3 + "s ";
+
+  document.getElementById("time4").innerHTML =
+    days4 + "d " + hours4 + "h " + minutes4 + "m " + seconds4 + "s ";
+
+  document.getElementById("time5").innerHTML =
+    days5 + "d " + hours5 + "h " + minutes5 + "m " + seconds5 + "s ";
+
+  document.getElementById("time6").innerHTML =
+    days6 + "d " + hours6 + "h " + minutes6 + "m " + seconds6 + "s ";
+
+  document.getElementById("time7").innerHTML =
+    days7 + "d " + hours7 + "h " + minutes7 + "m " + seconds7 + "s ";
+
+  document.getElementById("time8").innerHTML =
+    days8 + "d " + hours8 + "h " + minutes8 + "m " + seconds8 + "s ";
+
+  document.getElementById("time9").innerHTML =
+    days9 + "d " + hours9 + "h " + minutes9 + "m " + seconds9 + "s ";
+
+  document.getElementById("time10").innerHTML =
+    days10 + "d " + hours10 + "h " + minutes10 + "m " + seconds10 + "s ";
+
+  // If the count down is over, write some text
+  if (distance1 < 0) {
+    clearInterval(x);
+    document.getElementById("time1").innerHTML = "Available Now";
+  }
+  if (distance2 < 0) {
+    clearInterval(x);
+    document.getElementById("time2").innerHTML = "Available Now";
+  }
+  if (distance3 < 0) {
+    clearInterval(x);
+    document.getElementById("time3").innerHTML = "Available Now";
+  }
+  if (distance4 < 0) {
+    clearInterval(x);
+    document.getElementById("time4").innerHTML = "Available Now";
+  }
+  if (distance5 < 0) {
+    clearInterval(x);
+    document.getElementById("time5").innerHTML = "Available Now";
+  }
+  if (distance6 < 0) {
+    clearInterval(x);
+    document.getElementById("time6").innerHTML = "Available Now";
+  }
+  if (distance7 < 0) {
+    clearInterval(x);
+    document.getElementById("time7").innerHTML = "Available Now";
+  }
+  if (distance8 < 0) {
+    clearInterval(x);
+    document.getElementById("time8").innerHTML = "Available Now";
+  }
+  if (distance9 < 0) {
+    clearInterval(x);
+    document.getElementById("time9").innerHTML = "Available Now";
+  }
+  if (distance10 < 0) {
+    clearInterval(x);
+    document.getElementById("time10").innerHTML = "Available Now";
+  }
+}, 1000);
